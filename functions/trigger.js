0b762ddd1b7354351db45daacd765d197e4b8d93
Jason Lengstorf <jason@lengstorf.com>
2020-11-19 10:46:18 -0800

    fix: oops

--- functions/trigger.js
+++ functions/trigger.js
@@ -2,7 +2,9 @@ exports.handler = async (event) => {
   // {"session_variables":{"x-hasura-role":"admin"},"input":{"numbers":[10,5]},"action":{"name":"add"}}
   const body = JSON.parse(event.body);
 
-  const { [num1, num2] } = body.input;
+  const {
+    numbers: [num1, num2],
+  } = body.input;
 
   // TODO respond to Hasura
   return {
