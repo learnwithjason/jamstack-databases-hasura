eb823d169a0fc0799aca69298f279a1f90dc3031
Jason Lengstorf <jason@lengstorf.com>
2020-11-19 11:54:58 -0800

    feat: docs and cleanup

--- functions/trigger.js
+++ functions/trigger.js
@@ -1,4 +1,5 @@
 exports.handler = async (event) => {
+  // the body contains something like this:
   // {"session_variables":{"x-hasura-role":"admin"},"input":{"numbers":[10,5]},"action":{"name":"add"}}
   const body = JSON.parse(event.body);
 
@@ -6,7 +7,6 @@ exports.handler = async (event) => {
     numbers: [num1, num2],
   } = body.input;
 
-  // TODO respond to Hasura
   return {
     statusCode: 200,
     body: JSON.stringify({ sum: num1 + num2 }),
--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,10 +1,10 @@
 module.exports = {
   // keep it short! shown in the header
-  title: 'Demo of the Demo',
+  title: 'Databases for Jamstack Sites and Apps',
 
   // these are all optional and add links to the footer
-  repo: 'learnwithjason/demo-base',
+  repo: 'learnwithjason/jamstack-databases-hasura',
   episode:
-    'https://www.learnwithjason.dev/creating-css-variable-font-text-effects',
-  tutorial: 'https://codepen.io/jlengstorf/pen/QWbdLjb',
+    'https://www.learnwithjason.dev/databases-for-jamstack-sites-and-apps',
+  // tutorial: 'https://codepen.io/jlengstorf/pen/QWbdLjb',
 };
